fdc208bc1b105172e95032e324fb0dfa5f2b6a96
Codemupf <mupengfei6688@163.com>
2018-06-11 19:32:55 +0800

    httpTool created

---
+++ src/main/java/com/Oovever/easyHttp/Status.java
@@ -0,0 +1,13 @@
+package com.Oovever.easyHttp;
+
+/**
+ * Http返回状态码
+ * @author OovEver
+ * 2018/6/11 19:28
+ */
+public interface Status {
+    /**
+     * HTTP Status-Code 200: OK.
+     */
+    public static final int HTTP_OK = 200;
+}
